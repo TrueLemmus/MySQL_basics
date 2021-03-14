e833ce17cea54a4068f40d4676f9354012d2627c
TrueLemmus <sarekd@gmail.com>
2021-03-14 16:12:41 +0300

    home work 3 done

---
+++ lesson3/task2.sql
@@ -0,0 +1,29 @@
+/* Практическое задание №3. */
+
+/* Задание 2.
+ * Придумать 2-3 таблицы для БД vk, которую мы создали на занятии (с перечнем полей, указанием индексов и внешних ключей).
+*/
+
+-- выбираем текущую базу с которой будем работать
+USE vk;
+
+-- таблица постов пользователя
+CREATE TABLE user_posts (
+	post_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	user_id BIGINT UNSIGNED NOT NULL,
+	created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	post_txt TEXT NOT NULL,
+	INDEX fk_user_post_idx (user_id),
+	CONSTRAINT fk_user_post FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+-- таблица чёрного списка
+CREATE TABLE blacklist (
+  blacklist_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id BIGINT UNSIGNED NOT NULL,
+  banned_user_id BIGINT UNSIGNED NOT NULL,
+  INDEX fk_blacklist_user_id_idx (user_id),
+  INDEX fk_blacklist_banned_user_id_idx (banned_user_id),
+  CONSTRAINT fk_blacklist_user_id FOREIGN KEY (user_id) REFERENCES users (id),
+  CONSTRAINT fk_blacklist_banned_user_id FOREIGN KEY (banned_user_id) REFERENCES users (id)
+);
